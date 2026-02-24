abd6419b1bc74e4c2aee8658132ff9bce3c307ec
heonjuun <s01077314486@gmail.com>
2026-02-24 16:59:20 +0900

    추가
    배열 그외 등등

---
+++ Array.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+//첫번째(시작)으로부터 0번째부터 떨어져있는 위치부터 [0].........[9]
+int main()
+{
+	//메모리 공간잡고 산술연산
+	//간단한 로직을 반복 작업
+	int won[10];
+	
+	
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	won[i] = 10;		
+	//	cout << won[i]<<endl;
+	//}
+	
+	//ranged for 범위기반 for문
+	/*for (int data:won)
+	{
+
+	}*/
+	
+	/**(won + 0) = 42;
+	*(won + 1) = 12;*/
+
+	//4byte 2 ^ 32= -2G 0 ~ 2G
+	int MoneyOfKim=0;
+	//자료형 변수명 초기화(메모리에 올릴때 초기화된상태(0)로 시작)
+	//상수 작업 x ->대부분 사용자가 사용하는 프로그램은 입력으로 이루어져있기 때문.
+
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	cout << "두번째 작업물" << endl;	
-	cout << 52 / 2 << endl;
-	cout << 2 << endl;
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//cout << "두번째 작업물" << endl;
+//	//cout << 52 / 2 << endl;
+//	//cout << 2 << endl;
+//	cout << 'ABCD';
+//	// 'A' c언어에서는 이것을 숫자로 본다. 문자열로 표기하고 싶으면 ""
+//	//문자 상수 'A','B'......'Z'
+//	// 변수를 선언하면 메모리에 무작위로 공간을 만들어 별명을 정해서 기록,  ex)int money;선언했는데 Ram(Main Memory)에 money[10]
+//	int BankOfGold; //언리얼엔진은 카멜,스네이크 표기안씀, 단어가 바뀔때마다 대문자로 첫머리 표기(파스칼 표기법)파스칼 케이스
+//	//단어를 다 표기함 줄임말없이
+//
+//	//생략 절대 하지마
+//	//왜될까?생각합시다.(설명할수있을정도)
+//	int DailiyMoney;
+//	int Days;
+//
+//	return 0;
+//}
